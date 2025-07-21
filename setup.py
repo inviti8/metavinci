77e7384cedb8132b41a28d703b5c0bc60e4d1be3
Jason <dimensions3@outlook.com>
2025-07-20 20:43:26 -0700

    workflow WIP

--- setup.py
+++ setup.py
@@ -1,5 +1,40 @@
 from cx_Freeze import setup, Executable
 
+shortcut_table = [
+    (
+        "DesktopShortcut",        # Shortcut
+        "DesktopFolder",          # Directory_
+        "Metavinci",              # Name
+        "TARGETDIR",              # Component_
+        "[TARGETDIR]metavinci.exe", # Target
+        None,                     # Arguments
+        "Metavinci Desktop App",  # Description
+        None,                     # Hotkey
+        None,                     # Icon
+        None,                     # IconIndex
+        None,                     # ShowCmd
+        'TARGETDIR'               # WkDir
+    ),
+    (
+        "StartMenuShortcut",      # Shortcut
+        "ProgramMenuFolder",      # Directory_
+        "Metavinci",              # Name
+        "TARGETDIR",              # Component_
+        "[TARGETDIR]metavinci.exe", # Target
+        None,                     # Arguments
+        "Metavinci Desktop App",  # Description
+        None,                     # Hotkey
+        None,                     # Icon
+        None,                     # IconIndex
+        None,                     # ShowCmd
+        'TARGETDIR'               # WkDir
+    ),
+]
+
+msi_data = {
+    "Shortcut": shortcut_table,
+}
+
 setup(
     name="Metavinci",
     version="0.1",
@@ -12,8 +47,7 @@ setup(
         "bdist_msi": {
             "add_to_path": False,
             "initial_target_dir": r"[ProgramFilesFolder]\\Metavinci",
-            "shortcut_name": "Metavinci",
-            "shortcut_dir": "ProgramMenuFolder",
         }
-    }
+    },
+    msi_data=msi_data,
 ) 
